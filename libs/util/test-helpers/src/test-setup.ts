c9ef0f5811453e9b92e9cf9fb3cc09a28bace4ee
adrian-romanski <muszynianka1337@gmail.com>
2026-03-14 10:00:59 +0100

    chore: add axe core support to generator for regression testing

---
+++ libs/util/test-helpers/src/test-setup.ts
@@ -0,0 +1,6 @@
+import '@angular/compiler';
+import '@analogjs/vitest-angular/setup-snapshots';
+import 'vitest-canvas-mock';
+import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
+
+setupTestBed();
